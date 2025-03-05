e3e496a67010e0890da30903c4f96549652064d6
hieunv-0908 <nn650370@gmail.com>
2025-03-05 23:31:21 +0700

    Update bai15.js

--- bai21/baitap/bai15.js
+++ bai21/baitap/bai15.js
@@ -1,4 +1,4 @@
-var menhGia = [500000, 200000, 100000, 50000, 20000, 10000, 5000, 2000, 1000]; // Danh sách các mệnh giá
+var menhGia = [500000, 200000, 100000, 50000, 20000, 10000, 5000, 2000, 1000];
 var soTien = parseInt(prompt("Nhập số tiền cần rút (phải chia hết cho 1000):"));
 
 if (soTien % 1000 !== 0 || isNaN(soTien) || soTien <= 0) {
@@ -6,7 +6,7 @@ if (soTien % 1000 !== 0 || isNaN(soTien) || soTien <= 0) {
 } else {
     document.write("<h2>Kết quả rút tiền:</h2>");
     for (var i = 0; i < menhGia.length; i++) {
-        var soTo = Math.floor(soTien / menhGia[i]); // Số tờ của mệnh giá hiện tại
+        var soTo = Math.floor(soTien / menhGia[i]); 
         if (soTo > 0) {
             document.write(menhGia[i].toLocaleString("vi-VN") + " - " + soTo + " tờ<br>");
             soTien -= soTo * menhGia[i]; // Giảm số tiền đã rút
