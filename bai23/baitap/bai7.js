ae856eff56cfcc658d60d15be51debb316c4852d
hieunv-0908 <nn650370@gmail.com>
2025-03-09 22:17:26 +0700

    Update bai7.js

--- bai23/baitap/bai7.js
+++ bai23/baitap/bai7.js
@@ -1,35 +1,39 @@
 let arr = [];
-let n = +prompt("Mời bạn nhập vào số lượng phần tử trong mảng :");
-if (n === 0) {
-    alert("Mảng không có phần tử nào.");
-} else if (n < 0) {
-    alert("Số lượng phần tử trong mảng không được nhỏ hơn 0.");
-} else if (isNaN(n)) {
+let n = +prompt("Mời bạn nhập vào số lượng phần tử trong mảng:");
+if (isNaN(n) || n < 0) {
     alert("Số lượng phần tử trong mảng không hợp lệ.");
+} else if (n === 0) {
+    alert("Mảng không có phần tử nào.");
 } else {
     for (let i = 0; i < n; i++) {
-        arr[i] = +prompt(`Mời bạn nhập vào phàn tử thứ ${i + 1} (NHập vào -1000 để dừng trương trình)`);
-        if (isNaN(arr[i])) {
+        let num = prompt(`Mời bạn nhập vào phần tử thứ ${i + 1} (Nhập -1000 để dừng chương trình)`);
+        num = num.trim();
+        if (num === "-1000") break;
+        num = +num;
+        if (!isNaN(num)) {
+            arr.push(num);
+        } else {
+            alert("Vui lòng nhập số hợp lệ!");
             i--;
-        } else if (arr[i] === -1000) {
-            break;
         }
     }
-    let num_Max = arr[0];
-    let index_Max = 0;
-    for (let i = 0; i < arr.length - 1; i++) {
-        if (num_Max < arr[i + 1]) {
-            num_Max = arr[i + 1];
-            index_Max = i + 1;
+
+    if (arr.length < 2) {
+        alert("Không có số lớn thứ hai vì mảng có ít hơn hai phần tử hợp lệ.");
+    } else {
+        let max1 = -Infinity, max2 = -Infinity;
+        for (let i = 0; i < arr.length; i++) {
+            if (arr[i] > max1) {
+                max2 = max1;
+                max1 = arr[i];
+            } else if (arr[i] > max2 && arr[i] < max1) {
+                max2 = arr[i];
+            }
+        }
+        if (max2 === -Infinity) {
+            alert("Không có số lớn thứ hai vì tất cả phần tử đều giống nhau.");
+        } else {
+            alert("Số lớn thứ hai là: " + max2);
         }
     }
-    arr.splice(index_Max, 1);
-    num_Max = arr[0];
-    for (let i = 0; i < arr.length - 1; i++) {
-        if (num_Max < arr[i + 1]) {
-            num_Max = arr[i + 1];
-            index_Max = i + 1;
-        }
-    }
-    alert(num_Max);
 }
